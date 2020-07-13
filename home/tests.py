da498a44b8288e577dcb21ad1fbd1b317fba6b48
Yvette Smith <yvette@boesman.nl>
2020-07-13 20:25:46 +0000

    setup products app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestHome(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+           
+    def test_home_url_exists_at_desired_location(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
