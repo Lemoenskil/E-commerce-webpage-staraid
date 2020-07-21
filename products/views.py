7753f7222e4dd943d9eabe3aff154b7b0d8752db
Yvette Smith <yvette@boesman.nl>
2020-07-21 21:25:15 +0000

    adding featured products to index

--- products/views.py
+++ products/views.py
@@ -3,6 +3,5 @@ from .models import Product
 
 # Create your views here.
 def all_products(request):
-    print('got here')
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
