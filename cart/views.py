ce5344d689b193e51265f81c6b518cc82fc1380d
Yvette Smith <yvette@boesman.nl>
2020-07-24 17:20:44 +0000

    Fixes cart item image and quantity adjustment

--- cart/views.py
+++ cart/views.py
@@ -22,7 +22,7 @@ def adjust_cart(request, id):
     Adjust the quantity of the specified product to the specified
     amount
     """
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity' + id))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
