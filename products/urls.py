d95e3834628a3585914206370fa28c450b16afd1
Yvette Smith <yvette@boesman.nl>
2020-08-01 10:50:21 +0000

    setup single product page

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include, re_path
-from .views import all_products
+from .views import all_products, single_product
 from django.urls import path, re_path
 
 urlpatterns = [
     re_path(r'^$', all_products, name='products'),
+    re_path(r'^(?P<id>.+)$', single_product, name='single_product'),
 ]
 
 
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+    
+def single_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, "single_product.html", {"product": product})
