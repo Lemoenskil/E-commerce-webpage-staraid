ef2f5346c9f25890021b12d386e5689bf5cf00da
Yvette Smith <yvette@boesman.nl>
2020-08-08 12:41:03 +0000

    add allowed host for heroku

--- staraid_ecommerce/settings.py
+++ staraid_ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ os.environ.get('C9_HOSTNAME'), 'c21e9a61c10a4e7283367b2abc1b0016.vfs.cloud9.eu-central-1.amazonaws.com' ]
+ALLOWED_HOSTS = [ os.environ.get('C9_HOSTNAME'), 'c21e9a61c10a4e7283367b2abc1b0016.vfs.cloud9.eu-central-1.amazonaws.com', 'staraid-ecommerce.herokuapp.com']
 
 # Application definition
 
